63db60f98485c5d11d5c8938a7933fcb00fa6435
Naeem Khedarun <naeem.khedarun@gmail.com>
2015-04-06 19:22:47 +0100

    Add powershell cmdlet.

---
+++ src/Configuratively/Configuratively.psm1
@@ -0,0 +1,15 @@
+﻿function Save-Configuration
+{
+	param
+	(
+		[Parameter(Mandatory=$true)]
+		[string]$repositoryPath,
+		[Parameter(ParameterSetName="export",Mandatory=$true)]
+		[string]$route,
+		[Parameter(ParameterSetName="export",Mandatory=$true)]
+		[string]$outputPath
+	)
+
+	& $PSScriptRoot\Configuratively.exe export $repositoryPath $route $outputPath
+
+}
