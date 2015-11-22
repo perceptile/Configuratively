68dcab5daba798c54da6c7eb83500457845c523e
James Dawson <james@readsource.co.uk>
2015-11-22 22:44:26 +0000

    Added more scenarios to the token resolution tests

--- src/Configuratively.Tests/RecursiveTokenResolutionTests.cs
+++ src/Configuratively.Tests/RecursiveTokenResolutionTests.cs
@@ -21,7 +21,10 @@ namespace Configuratively.Tests
 
 
             var tokenResolver = new TokenResolver();
-            repo.Repo = tokenResolver.ResolveTokens(repo.Repo);
+            for (int i = 0; i < repo.Repo.Length; i++)
+            {
+                repo.Repo[i] = tokenResolver.ResolveTokens(repo.Repo[i]);
+            }
 
             // These tests are only for the core token replacement functionality
             // within a single hierarchy (i.e. JSON document tree), hence the indexing on repo.Repo
@@ -38,6 +41,14 @@ namespace Configuratively.Tests
             // tokenised value referencing an item that itself is tokenised also (i.e. a nested tokenisation)
             Assert.AreEqual("bar", repo.Repo[1].simple4.ToString());
 
+            // tokenised value referencing an item that is nested deeply in the object graph
+            Assert.AreEqual("foobar", repo.Repo[1].simple6.ToString());
+
+            // tokenised value referencing an item that is in a linked file
+            Assert.AreEqual("commonTokenValue", repo.Repo[1].simple7.ToString());
+
+            // tokenised value referencing an item that is in a linked file and nested deeply in the object graph
+            Assert.AreEqual("nestedValue", repo.Repo[1].simple8.ToString());
         }
     }
 }
